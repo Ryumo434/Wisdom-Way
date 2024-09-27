d549075023d87290f62cb5086e0a3dbc4f47410c
Ryumo434 <158557069+Ryumo434@users.noreply.github.com>
2024-09-27 20:52:39 +0200

    EnemyAi Follow with range + Distance
    Die Feinde können den Spieler nun verfolgen und lassen ab einem bestimmten Radius von ihm ab

---
+++ Assets/Scripts/Enemies/AIChase.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIChase : MonoBehaviour
+{
+
+    public GameObject player;
+    public float ChaseSpeed;
+    public float ChaseDistance;
+
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {               // Distance berechnet den Abstand zwischen zwei gameobjects und gibt diesen als Float zurück
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        //Math.Atan2 wird verwendet um den winkel von zwei objekten zu berechnen
+        //Rad2Deg konvertiert Radiant zu Degree
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+
+
+
+        if (distance < ChaseDistance)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, ChaseSpeed * Time.deltaTime);
+            // transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+    }
+}
