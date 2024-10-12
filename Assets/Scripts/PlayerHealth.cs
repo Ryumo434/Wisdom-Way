9d635933b8a7d20d36dd8239343f2a57a251098b
Ryumo434 <158557069+Ryumo434@users.noreply.github.com>
2024-10-12 11:47:47 +0200

    ScreenShake
    bei erleiden von Schaden wackelt der Bildschirm (eventuell zu hart)

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -38,6 +38,8 @@ public class PlayerHealth : MonoBehaviour
 
     private void TakeDamage(int damageAmount)
     {
+        ScreenShakeManager.Instance.ShakeScreen();
+
         canTakeDamage = false;
         currentHealth -= damageAmount;
         StartCoroutine(DamageRecoveryRoutine());
