11edc2ad85d52ea61c30f6410979017c91ea040f
lukas <82029186+42lukas@users.noreply.github.com>
2024-10-15 19:53:24 +0200

    press e progress

---
+++ Assets/Scripts/World Mechanics/PressEProgress.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.UI; // F?r UI-Elemente
+
+public class PressEProgress : MonoBehaviour
+{
+    public Slider progressBar; // Referenz zum UI-Slider als Fortschrittsanzeige
+    public float totalPressTimeRequired = 15f; // Gesamtzeit, die 'e' gedr?ckt werden muss
+
+    private bool isPlayerInTrigger = false;
+    private float ePressTime = 0f;
+
+    private void Start()
+    {
+        if (progressBar != null)
+        {
+            progressBar.maxValue = totalPressTimeRequired;
+            progressBar.value = 0f;
+            progressBar.gameObject.SetActive(false); // Fortschrittsanzeige zu Beginn ausblenden
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) // Verwende OnTriggerEnter2D f?r 2D-Collider
+    {
+        if (other.CompareTag("Player")) // Annahme: Der Spieler hat den Tag "Player"
+        {
+            isPlayerInTrigger = true;
+            if (progressBar != null)
+            {
+                progressBar.gameObject.SetActive(true); // Fortschrittsanzeige einblenden
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) // Verwende OnTriggerExit2D f?r 2D-Collider
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+            ePressTime = 0f; // Fortschritt zur?cksetzen
+            if (progressBar != null)
+            {
+                progressBar.value = 0f;
+                progressBar.gameObject.SetActive(false); // Fortschrittsanzeige ausblenden
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerInTrigger)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                // Wenn die Taste gedr?ckt wird, den Fortschritt erh?hen
+                ePressTime += Time.deltaTime;
+                if (ePressTime >= totalPressTimeRequired)
+                {
+                    ePressTime = totalPressTimeRequired;
+                    // Aktion bei Erreichen der 15 Sekunden
+                    Debug.Log("'e' wurde f?r 15 Sekunden gedr?ckt!");
+                }
+            }
+            else
+            {
+                // Fortschritt zur?cksetzen, wenn 'e' losgelassen wird
+                ePressTime = 0f;
+            }
+
+            // Fortschrittsanzeige aktualisieren
+            if (progressBar != null)
+            {
+                progressBar.value = ePressTime;
+            }
+        }
+    }
+}
