f86a71200d7fcba6af6eef21ec7206cf74155e94
lukas <82029186+42lukas@users.noreply.github.com>
2025-01-31 14:39:21 +0100

    shop finally implemented

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -8,55 +8,8 @@ public class Shop : MonoBehaviour
     [Header("Drag & Drop vom E-Text")]
     [SerializeField] private GameObject eText;
 
-    private static Shop instance; // Singleton-Instanz
     private bool isPlayerInTrigger = false;
 
-    void Awake()
-    {
-        // Singleton sicherstellen
-        if (instance != null && instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
-
-    private void Start()
-    {
-        AssignReferences();
-    }
-
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        AssignReferences();
-    }
-
-    private void AssignReferences()
-    {
-        // UI finden, falls Referenz fehlt
-        if (ui == null)
-            ui = GameObject.Find("UICanvas/Shop Container");
-
-        if (ui == null)
-            Debug.LogError($"{name}: Keine UI-Referenz gefunden!");
-
-        if (eText == null)
-            Debug.LogError($"{name}: Kein E-Text gefunden!");
-        else
-            eText.SetActive(false);
-    }
 
     private void Update()
     {
