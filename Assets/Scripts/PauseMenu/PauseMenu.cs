f87a5d0eaa34ebe6d37830c10e62b28a7866a632
snaan5 <siyur2005@gmail.com>
2025-02-04 16:31:11 +0100

    PauseMenuScript

---
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+
+
+
+
+public class PauseMenuScript : MonoBehaviour
+
+{
+
+    bool gamePaused = false;
+
+    [SerializeField] GameObject pauseMenu;
+
+
+
+    void Update()
+
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == false)
+
+        {
+
+            Time.timeScale = 0;
+
+            gamePaused = true;
+
+            pauseMenu.SetActive(true);
+
+        }
+
+        else if ((Input.GetKeyDown(KeyCode.Escape) && gamePaused == true))
+
+        {
+
+            Time.timeScale = 1;
+
+            gamePaused = false;
+
+            pauseMenu.SetActive(false);
+
+        }
+
+    }
+
+
+
+    public void Home()
+
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+
+
+    public void Continue()
+
+    {
+
+        Time.timeScale = 1;
+
+        gamePaused = false;
+
+        pauseMenu.SetActive(false);
+
+    }
+
+}
