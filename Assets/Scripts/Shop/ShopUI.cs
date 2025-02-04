af1ba7e6b7c0464745fffefd5faf99f2b588dd5d
lukas <82029186+42lukas@users.noreply.github.com>
2025-02-04 21:58:35 +0100

    added shop filter options

--- Assets/Scripts/Shop/ShopUI.cs
+++ Assets/Scripts/Shop/ShopUI.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 public class ShopUI : MonoBehaviour
 {
     [Header("Grid/ScrollView-Einstellungen")]
-    [SerializeField] private Transform contentParent;  // Z.B. das "Content"-Objekt der ScrollView
-    [SerializeField] private GridLayoutGroup gridLayout; // Referenz auf das GridLayoutGroup-Objekt
+    [SerializeField] private Transform contentParent;
+    [SerializeField] private GridLayoutGroup gridLayout;
 
     [Header("Prefab-Einstellungen")]
-    [SerializeField] private GameObject shopItemPrefab; // Unser ShopItemUI-Prefab
+    [SerializeField] private GameObject shopItemPrefab;
 
     [Header("Inventar-Einstellungen")]
-    [SerializeField] private List<ShopItem> shopInventory; // Deine Shop-Items
+    [SerializeField] private List<ShopItem> shopInventory;
 
     private void Start()
     {
@@ -27,18 +28,81 @@ public class ShopUI : MonoBehaviour
             Destroy(child.gameObject);
         }
 
-        // Shop-Items hinzuf?gen
+        // Shop-Items hinzufügen
         foreach (ShopItem item in shopInventory)
         {
-            // Prefab instanziieren
             GameObject newItemUI = Instantiate(shopItemPrefab, contentParent);
 
-            // ShopItemUI-Komponente holen
             ShopItemUI itemUI = newItemUI.GetComponent<ShopItemUI>();
             if (itemUI != null)
             {
                 // Hier kriegt das UI seine Daten
                 itemUI.Initialize(item);
+            }
+        }
+    }
+    public void OnButtonClick(Button clickedButton)
+    {
+        if (clickedButton.tag == "weapon")
+        {
+            filterWeapons();
+        } else if (clickedButton.tag == "potion")
+        {
+            filterPotions();
+        } else
+        {
+            PopulateShop();
+        }
+    }
+
+    private void filterPotions()
+    {
+        // Vorherigen Inhalt entfernen
+        foreach (Transform child in contentParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Shop-Items hinzufügen
+        foreach (ShopItem item in shopInventory)
+        {
+            if (item.Description == "potion")
+            {
+                GameObject newItemUI = Instantiate(shopItemPrefab, contentParent);
+
+                // ShopItemUI-Komponente holen
+                ShopItemUI itemUI = newItemUI.GetComponent<ShopItemUI>();
+                if (itemUI != null)
+                {
+                    // Hier kriegt das UI seine Daten
+                    itemUI.Initialize(item);
+                }
+            }
+        }
+    }
+
+    private void filterWeapons()
+    {
+        // Vorherigen Inhalt entfernen
+        foreach (Transform child in contentParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Shop-Items hinzufügen
+        foreach (ShopItem item in shopInventory)
+        {
+            if (item.Description == "weapon")
+            {
+                GameObject newItemUI = Instantiate(shopItemPrefab, contentParent);
+
+                // ShopItemUI-Komponente holen
+                ShopItemUI itemUI = newItemUI.GetComponent<ShopItemUI>();
+                if (itemUI != null)
+                {
+                    // Hier kriegt das UI seine Daten
+                    itemUI.Initialize(item);
+                }
             }
         }
     }
