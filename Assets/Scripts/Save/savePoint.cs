f687139cdf2a83fd98b7f234a10947d0883b2b31
Mihoshi <74931190+Srakawichi@users.noreply.github.com>
2025-02-18 16:51:10 +0100

    Place Save Point
    DontDestroyOnload Problem

--- Assets/Scripts/Save/savePoint.cs
+++ Assets/Scripts/Save/savePoint.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SavePoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public GameObject SavePointPanel;
+
+    private void Start()
     {
-        
+    
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SavePointPanel.SetActive(true);
+            Debug.Log("Player hat den Collider betreten.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            SavePointPanel.SetActive(false);
+            Debug.Log("Player hat den Collider verlassen.");
+        }
     }
 }
