d0e1f3650972200103bb99a7a138b33aaee81677
Mihoshi <74931190+Srakawichi@users.noreply.github.com>
2025-02-18 19:03:33 +0100

    SaveButton

--- Assets/Scripts/Save/savePoint.cs
+++ Assets/Scripts/Save/savePoint.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SavePoint : MonoBehaviour
 {
-    [SerializeField] public GameObject SavePointPanel;
+    [SerializeField] private GameObject SavePointPanel;
+    [SerializeField] private Button SaveButton;  
+    [SerializeField] private GameObject SpawnPoint;
 
-    private void Start()
+   private void Start()
     {
         // Finde das Root-Objekt "UICanvas"
         GameObject uiCanvas = GameObject.Find("UICanvas");
+        GameObject SpawnPoint = GameObject.Find("SpawnPoint");
 
         if (uiCanvas != null)
         {
@@ -20,6 +24,34 @@ public class SavePoint : MonoBehaviour
             {
                 SavePointPanel = savePointPanelTransform.gameObject;
                 DontDestroyOnLoad(SavePointPanel);
+
+                // Finde den SaveButton und füge die Click-Event-Listener hinzu
+                Transform backgroundTransform = savePointPanelTransform.Find("Background");
+                if (backgroundTransform != null)
+                {
+                    Transform saveButtonTransform = backgroundTransform.Find("SaveButton");
+                    if (saveButtonTransform != null)
+                    {
+                        SaveButton = saveButtonTransform.GetComponent<Button>();
+                        if (SaveButton != null)
+                        {
+                            SaveButton.onClick.AddListener(OnSaveButtonClicked);
+                            Debug.Log("SaveButton wurde erfolgreich gefunden und Listener hinzugefügt.");
+                        }
+                        else
+                        {
+                            Debug.LogError("SaveButton-Komponente wurde nicht gefunden!");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("SaveButton unter Background nicht gefunden!");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Background unter SavePointPanel nicht gefunden!");
+                }
             }
             else
             {
@@ -30,6 +62,13 @@ public class SavePoint : MonoBehaviour
         {
             Debug.LogError("UICanvas konnte nicht gefunden werden!");
         }
+    }
+
+    private void OnSaveButtonClicked()
+    {
+        SavePointPanel.SetActive(false);
+        SpawnPoint.SetActive(false);
+        Debug.Log("Save Button wurde gedrückt!");
     }
 
     
