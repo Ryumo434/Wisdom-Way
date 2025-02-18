7260a580709b358668a6c2f70a83a4120533bd43
Mihoshi <74931190+Srakawichi@users.noreply.github.com>
2025-02-18 17:36:13 +0100

    Update savePoint.cs

--- Assets/Scripts/Save/savePoint.cs
+++ Assets/Scripts/Save/savePoint.cs
@@ -8,12 +8,34 @@ public class SavePoint : MonoBehaviour
 
     private void Start()
     {
-    
+        // Finde das Root-Objekt "UICanvas"
+        GameObject uiCanvas = GameObject.Find("UICanvas");
+
+        if (uiCanvas != null)
+        {
+            // Suche nach dem Kindobjekt "SavePointPanel" innerhalb von "UICanvas"
+            Transform savePointPanelTransform = uiCanvas.transform.Find("SavePointPanel");
+
+            if (savePointPanelTransform != null)
+            {
+                SavePointPanel = savePointPanelTransform.gameObject;
+                DontDestroyOnLoad(SavePointPanel);
+            }
+            else
+            {
+                Debug.LogError("SavePointPanel konnte unter UICanvas nicht gefunden werden!");
+            }
+        }
+        else
+        {
+            Debug.LogError("UICanvas konnte nicht gefunden werden!");
+        }
     }
+
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && SavePointPanel != null)
         {
             SavePointPanel.SetActive(true);
             Debug.Log("Player hat den Collider betreten.");
@@ -22,7 +44,7 @@ public class SavePoint : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && SavePointPanel != null)
         {
             SavePointPanel.SetActive(false);
             Debug.Log("Player hat den Collider verlassen.");
