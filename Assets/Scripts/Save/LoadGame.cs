5995aeab06a2366ae1da251b3d23c6ff25a93c9a
Mihoshi <74931190+Srakawichi@users.noreply.github.com>
2025-02-27 12:44:58 +0100

    Update LoadGame.cs

--- Assets/Scripts/Save/LoadGame.cs
+++ Assets/Scripts/Save/LoadGame.cs
@@ -1,49 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class LoadGame : MonoBehaviour
 {
     [SerializeField] private Button LoadButton;
-    //[SerializeField] private GameManager gameManager;
-
-    GameManager gameManager;
-   // [SerializeField] private GameObject SpawnPoint;
+    private string saveFilePath;
 
     void Start()
     {
-        //gameManager = GetComponent<GameManager>();
+        saveFilePath = Path.Combine(Application.persistentDataPath, "playerSave.json");
+        LoadButton.interactable = File.Exists(saveFilePath);
         LoadButton.onClick.AddListener(OnSaveButtonClicked);
     }
 
     private void OnSaveButtonClicked()
     {
-    /*    SavePointPanel.SetActive(false);
-        if (SpawnPoint != null)
+        if (GameManager.Instance != null)
         {
-            SpawnPoint.SetActive(false);
-            Debug.Log("SpawnPoint wurde deaktiviert.");
-        }
-        else
-        {
-            Debug.LogError("SpawnPoint wurde nicht gefunden!");
-        }
-        Debug.Log("Save Button wurde gedrückt!");  */
-        
-        //dieses if statemant ist unnnötig //Nasser
-        if (gameManager != null)
-        {
-            //gameManager.LoadGame();
             GameManager.Instance.LoadGame();
         }
         else
         {
-            
-           // Debug.LogError("GameManager wurde nicht gefunden!");
+            Debug.LogError("GameManager wurde nicht gefunden!");
         }
-
-
-        GameManager.Instance.LoadGame();
     }
 }
